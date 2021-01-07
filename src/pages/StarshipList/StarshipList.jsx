ffad4ae73c6b7d9b1226184faaa845ba375e39e9
jpaige343 <jenniferoneill129@gmail.com>
2021-01-07 16:49:17 -0600

    style ship list page

--- src/pages/StarshipList/StarshipList.jsx
+++ src/pages/StarshipList/StarshipList.jsx
@@ -1,35 +1,38 @@
-import React, { Component } from 'react';
-import {getAllStarships} from '../../services/sw-api';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import { getAllStarships } from "../../services/sw-api";
+import { Link } from "react-router-dom";
 
 class ShipList extends Component {
-  state = { 
-    results: []
-   }
-  async componentDidMount(){
-    const shipData = await getAllStarships()
-    this.setState({ results: shipData.results })
+  state = {
+    results: [],
+  };
+  async componentDidMount() {
+    const shipData = await getAllStarships();
+    this.setState({ results: shipData.results });
   }
-  render() { 
-    return ( 
+  render() {
+    return (
       <>
-      <h1>This is the Ship List Page</h1>
-      <div className="container">
-        {this.state.results.map((starship) => (
-          <div className="starships">
-            <Link
-              to={{
-                pathname: '/shipdetails',
-                state: {starship}
-              }}
-            >
-            {starship.name}
-            </Link>
-          </div>
-        ))}
-      </div>
+        <div className="title">
+          <h1>STAR WARS STARSHIPS</h1>
+        </div>
+        <div className="container">
+          {this.state.results.map((starship) => (
+            <div className="starships">
+              <Link
+                className="links"
+                to={{
+                  pathname: "/shipdetails",
+                  state: { starship },
+                }}
+              >
+                {starship.name}
+              </Link>
+            </div>
+          ))}
+        </div>
       </>
-     );
+    );
   }
 }
 
